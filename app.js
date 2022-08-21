93b7bb49380355b1b460ad9ee35271dbfa292e44
Arjun Das <arjundasback515@gmail.com>
2022-08-21 11:07:32 +0600

    calculatior button created and function added perticular btn

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ document.getElementById("genarate-btn").addEventListener("click",function(){
     displayPin.value=pin;
      
  })
+
+ document.getElementById("calculator").addEventListener("click",function(event){
+    const Number = event.target.innerText;
+    const numberValue = document.getElementById("input-number");
+    const previousValue = numberValue.value;
+    if(isNaN(Number)){
+        if(Number=="C"){
+            numberValue.value= "";
+        }
+        else if(Number=="<"){
+            const digit = previousValue.split('');
+            digit.pop();
+            const remaningDigit = digit.join("");
+            numberValue.value=remaningDigit;
+
+        }
+
+    }
+    else{
+        
+        
+        const newValue = previousValue + Number;
+        numberValue.value = newValue;
+    }
+ })
